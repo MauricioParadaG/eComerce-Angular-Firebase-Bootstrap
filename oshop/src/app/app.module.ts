f0018d37aacf434b9ee9dafbe4020f2ae9f637bd
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-03-19 23:39:11 +0100

    Creando componentes, instalando bootstrap y la base de datos firestore
    Creando componentes, instalando bootstrap y la base de datos firestore

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// FIRESTORE
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+//
+
+
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { AdminProductComponent } from './components/admin/admin-product/admin-product.component';
+import { AdminOrdersComponent } from './components/admin/admin-orders/admin-orders.component';
+import { ProductFormComponent } from './components/admin/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductComponent,
+    AdminOrdersComponent,
+    ProductFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- oshop/src/environments/environment.ts
+++ oshop/src/environments/environment.ts
@@ -1,16 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDOd8tr1FLXKcNWIWmgUs6DZrOIXiz04m4",
+    authDomain: "oshop-412b3.firebaseapp.com",
+    databaseURL: "https://oshop-412b3.firebaseio.com",
+    projectId: "oshop-412b3",
+    storageBucket: "oshop-412b3.appspot.com",
+    messagingSenderId: "822805620874",
+    appId: "1:822805620874:web:160227e5d152f8246b6573",
+    measurementId: "G-VFN3JR6WCY"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
