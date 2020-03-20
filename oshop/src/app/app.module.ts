cb705271435cdd4855ee9414865df52e47ee5143
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-03-20 09:39:01 +0100

    before instaling Bootstrap ng-bootstrap
    before instaling Bootstrap ng-bootstrap, I do a backup of the repository

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Route} from '@angular/router';
+
 // FIRESTORE
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireDatabaseModule } from "@angular/fire/database";
 import { AngularFireAuthModule } from "@angular/fire/auth";
-//
 
 
+// Components
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,9 +20,22 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
 import { CheckOutComponent } from './components/check-out/check-out.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { LoginComponent } from './components/login/login.component';
 import { AdminProductComponent } from './components/admin/admin-product/admin-product.component';
 import { AdminOrdersComponent } from './components/admin/admin-orders/admin-orders.component';
 import { ProductFormComponent } from './components/admin/product-form/product-form.component';
+
+
+const routes: Route[] = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent },
+  {path: 'shopping-cart', component: ShoppingCartComponent },
+  {path: 'check-out', component: CheckOutComponent },
+  {path: 'order-success', component: OrderSuccessComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'admin/products', component: AdminProductComponent },
+  {path: 'admin/orders', component: AdminProductComponent }
+];
 
 @NgModule({
   declarations: [
@@ -34,13 +49,15 @@ import { ProductFormComponent } from './components/admin/product-form/product-fo
     MyOrdersComponent,
     AdminProductComponent,
     AdminOrdersComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
