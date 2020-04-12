4704806fd22e2fffbc3c3c462f8eb4250c02b760
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-04-12 17:14:25 +0200

    Small change in Login, I just wanted to try a Popup login
    Small change in Login, I just wanted to try a Popup login and a small html change

--- oshop/src/app/services/auth.service.ts
+++ oshop/src/app/services/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
 login(){
   let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/' ;
   localStorage.setItem('returnUrl', returnUrl);
-  this.afireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  this.afireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
 }
 
 logout(){
